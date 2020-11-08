38ad22b1e5fc233f4944cb201336b41393c556da
SiAce <siacespark@gmail.com>
2020-11-08 12:59:58 -0500

    flask-restful finished (section 4)

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask
 from flask_jwt import JWT, jwt_required
+from flask_restful import Resource, Api, reqparse
 
 from security import authenticate, identity
 
@@ -8,12 +8,17 @@ app = Flask(__name__)
 app.secret_key = 'jose'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity) # /auth
+jwt = JWT(app, authenticate, identity)  # /auth
 
 items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True)
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -23,7 +28,8 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': 'An item with name: <{}> already exists.'.format(name)}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -32,6 +38,18 @@ class Item(Resource):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next((it for it in items if it['name'] == name), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+
+        return item
+
 
 class ItemList(Resource):
     def get(self):
--- section4/code/test.py
+++
@@ -1,5 +0,0 @@
-a = [1,2,3,44,5,3,6]
-
-g = next(e for e in a if e == 44)
-
-print(g)
